ae5345300bfc5f42308dae34ab5cba76c57b4ebc
신승훈 <>
2022-04-14 22:29:57 +0900

    feature : adImageHook 추가

--- hooks/useEditor.ts
+++ hooks/useEditor.ts
@@ -1,27 +1,37 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react';
 import Editor from '@toast-ui/editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
 const useEditor = () => {
-    const [editor,setEditor] = useState<Editor | null>(null);
-    useEffect(()=>{
-        const editorCore:Editor = new Editor({
-            el: document.querySelector('#editor') as HTMLElement,
-            previewStyle: 'vertical',
-            height: '700px',
-            initialValue:" ",
-            hooks:{
-                addImageBlobHook:(blob,callback) => {
-                    console.log(blob)
-                
-                }
+  const [editor, setEditor] = useState<Editor | null>(null);
+  useEffect(() => {
+    const editorCore: Editor = new Editor({
+      el: document.querySelector('#editor') as HTMLElement,
+      previewStyle: 'vertical',
+      height: '700px',
+      initialValue: ' ',
+      hooks: {
+        addImageBlobHook: (blob, callback) => {
+          const reader = new FileReader();
+          reader.readAsDataURL(blob);
+          reader.onload = () => {
+            if (reader.result == null) {
+              return;
             }
-          })
+            if (typeof reader.result !== 'string') {
+              return;
+            }
 
-        setEditor(editorCore)
-    },[])
+            callback(reader.result);
+          };
+        },
+      },
+    });
 
-    return editor
-}
+    setEditor(editorCore);
+  }, []);
 
-export default useEditor
+  return editor;
+};
+
+export default useEditor;
