e158ae6fc80c97460589479c83b268ffa77f9dbb
신승훈 <>
2022-04-18 23:03:19 +0900

    feature : varibale css 적용
    - scss 변수 적용

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Image from 'next/image';
 import styles from './Header.module.scss';
 
 function Header() {
   return (
     <header className={styles.container}>
       <nav>
-        <h1 className={styles.logo}>HooneyLog</h1>
+        <h1 className={styles.logo}>
+          HooneyLog<span className={styles.strong}> ★</span>
+        </h1>
       </nav>
     </header>
   );
