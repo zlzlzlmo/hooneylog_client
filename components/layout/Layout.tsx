d596c0e6013f102520674e570855fff3d470844d
신승훈 <>
2022-04-20 23:32:34 +0900

    head 추가

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react';
 import Content from './content/Content';
 import Footer from './footer/Footer';
@@ -7,6 +8,9 @@ import Introduce from './introduce/Introduce';
 const Layout: React.FC = ({ children }) => {
   return (
     <>
+      <Head>
+        <meta name="description" content="프론트엔드 개발자 신승훈이 직접 개발한 개인 기술 블로그입니다." />
+      </Head>
       <Header />
       {children}
       <Footer />
