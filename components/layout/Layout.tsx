d90bf97d776e8f953ac5ad8766c11c5c9cf0e393
신승훈 <>
2022-04-25 21:53:21 +0900

    meta 추가

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -7,7 +7,12 @@ const Layout: React.FC = ({ children }) => {
   return (
     <>
       <Head>
+        <meta property="og:title" content="후니로그" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://hooneylog.com" />
+        <meta property="og:locale" content="ko_KR" />
         <meta name="description" content="프론트엔드 개발자 신승훈이 직접 개발한 개인 기술 블로그입니다." />
+
         <link rel="icon" href="/favicon.png" />
       </Head>
       <Header />
