ae3919b0ee26fc6d8c298db226e10ae623784703
신승훈 <>
2022-05-11 22:41:54 +0900

    feature : test code
    - desktopcategroy filter 내 li 렌더링 갯수 체크
    - render block에서 bookmark type 추가

--- components/categoryFilter/desktop/DesktopCategoryFilter.test.tsx
+++ components/categoryFilter/desktop/DesktopCategoryFilter.test.tsx
@@ -15,20 +15,29 @@ jest.mock(`hooks/useCategoryFilter`, () => {
   }));
 });
 
-beforeEach(() => {
-  render(
-    <Provider store={store}>
-      <DesktopCategoryFilter />
-    </Provider>,
-  );
-});
-
 describe('<DesktopCategoryFilter/>', () => {
   test('초기 렌더시 all 카테고리가 active가 되어있는지 테스트', () => {
+    render(
+      <Provider store={store}>
+        <DesktopCategoryFilter />
+      </Provider>,
+    );
+
     const allCategory = screen.getByText(/all/i);
     screen.debug();
     expect(allCategory).toBeInTheDocument();
     expect(allCategory).toHaveTextContent('All');
     expect(allCategory).toHaveClass('active');
+  });
+
+  test('정상적인 갯수의 리스트가 렌더링 되는지 테스트', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <DesktopCategoryFilter />
+      </Provider>,
+    );
+
+    const list = container.querySelectorAll('li');
+    expect(list.length).toBe(5);
   });
 });
