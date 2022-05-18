98997b2a426e968fa9cfe5c58b631f27cd8dd808
신승훈 <>
2022-05-18 22:08:29 +0900

    feature : fbcomment
    - 페이스북 댓글 플러그인 컴포넌트 주석 제거

--- components/comment/FbComment.tsx
+++ components/comment/FbComment.tsx
@@ -15,7 +15,7 @@ const FbComment = ({ slug }: FbCommentProps) => {
 
   return (
     <>
-      <div className="fb-comments" data-href={`https://www.hooneylog.com/post/detail/${slug}`} data-width="100%" />
+      <div className="fb-comments" data-href={`https://www.hooneylog.com/post1/detail/${slug}`} data-width="100%" />
       <Script
         id="facebook-jssdk"
         src="https://connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v13.0&appId=540132141049632&autoLogAppEvents=1"
