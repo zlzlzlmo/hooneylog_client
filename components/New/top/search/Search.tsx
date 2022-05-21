e75c1667ca04730f4dc09f0b68cc32ab29fdff63
신승훈 <>
2022-05-21 19:13:16 +0900

    feature : search
    - 새로운 검색 컴포넌트에 검색 기능 추가

--- components/New/top/search/Search.tsx
+++ components/New/top/search/Search.tsx
@@ -1,16 +1,53 @@
-import React from 'react';
+/* eslint-disable no-use-before-define */
+import useHandleReduxData from 'hooks/useHandleReduxData';
+import useReduxData from 'hooks/useReduxData';
+import React, { ChangeEvent, FormEvent, useRef, useState } from 'react';
+import SearchController from 'util/search';
 import styles from './Search.module.scss';
 
 const Search = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [searchValue, setSearchValue] = useState('');
+  const { originalNotionList } = useReduxData();
+
+  const { dispatchFilterNotionList } = useHandleReduxData();
+
+  const handleSearchValue = () => (e: ChangeEvent<HTMLInputElement>) => setSearchValue(e.target.value);
+
+  const handleFormSubmit = () => (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    appendQueryString();
+    filterNotionListBySearchValue();
+    resetInputValue();
+
+    function appendQueryString() {
+      const searchParams = new URLSearchParams(window.location.search);
+      searchParams.set('search', searchValue);
+      window.history.pushState({}, '', `${window.location.origin}?${searchParams}`);
+    }
+
+    function filterNotionListBySearchValue() {
+      const filteredList = new SearchController(originalNotionList).filteredListBySearchValue(searchValue);
+      dispatchFilterNotionList(filteredList);
+    }
+
+    function resetInputValue() {
+      if (inputRef.current) {
+        inputRef.current.value = '';
+      }
+    }
+  };
   return (
     <section className={styles.container}>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log('ddd');
-        }}
-      >
-        <input type="text" className={styles.input} placeholder="검색어를 입력하세요" />
+      <form onSubmit={handleFormSubmit()}>
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="검색어를 입력하세요"
+          onChange={handleSearchValue()}
+          value={searchValue}
+          ref={inputRef}
+        />
         <button type="submit" className={styles.btn}>
           검색
         </button>
