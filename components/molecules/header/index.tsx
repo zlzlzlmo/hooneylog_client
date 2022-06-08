a126eaf7eadbad32b5e1f4c1cc81346c699a8956
신승훈 <zlzlzlmo@daum.net>
2022-06-08 21:23:27 +0900

    feat : . 제거
    - replace를 사용하여 . 제거

--- components/molecules/header/index.tsx
+++ components/molecules/header/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Logo from 'components/atoms/logo';
 import styles from './index.module.scss';
-import HeaderTextBox from '../headerTextBox';
 
 const Header = () => {
   return (
     <header className={styles.container}>
       <div className={styles.inner_header}>
         <Logo />
-        <HeaderTextBox />
       </div>
     </header>
   );
